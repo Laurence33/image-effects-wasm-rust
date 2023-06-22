3ddd6e0727d68c024a7d66e3391167cca0378c10
Laurence Cortez <laurencecortez26.lc@gmail.com>
2023-06-22 20:33:02 +0800

    image crate for modifying images

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use base64::decode;
+use image::load_from_memory;
 use wasm_bindgen::prelude::wasm_bindgen;
 use web_sys::console::log_1 as log;
 
@@ -7,4 +8,6 @@ pub fn grayscale(encoded_file: &str) {
     log(&"Grayscale called".into());
     let base64_to_vector = decode(encoded_file).unwrap();
     log(&"Image decoded".into());
+    let image = load_from_memory(&base64_to_vector).unwrap();
+    log(&"Image loaded".into());
 }
